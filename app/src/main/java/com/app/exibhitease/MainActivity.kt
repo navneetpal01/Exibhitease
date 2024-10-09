dcd02b6a410a33493851d55d479cff1a50c4df33
navneet <navneetpalgithub@gmail.com>
2024-10-09 18:16:07 +0530

    Updated the MainActivity

--- app/src/main/java/com/app/exibhitease/MainActivity.kt
+++ app/src/main/java/com/app/exibhitease/MainActivity.kt
@@ -6,13 +6,26 @@ import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.app.exibhitease.core.SettingsConstants.FIRST_LAUNCH
+import com.app.exibhitease.data.settings.SettingsDataStore
+import com.app.exibhitease.presentation.ExibhiteaseApp
+import com.app.exibhitease.presentation.settings_screen.SettingsViewModel
 import com.app.exibhitease.ui.theme.ExibhiteaseTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.runBlocking
+import javax.inject.Inject
 
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
+    private val settingsViewModel: SettingsViewModel by viewModels()
+
+    @Inject
+    lateinit var settingsDataStore: SettingsDataStore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,9 +35,20 @@ class MainActivity : ComponentActivity() {
                 Color.TRANSPARENT
             )
         )
-        setContent {
-            ExibhiteaseTheme {
+        val splashScreen = installSplashScreen()
+        splashScreen.setKeepOnScreenCondition { settingsViewModel.setKeepOnScreenCondition }
 
+        setContent {
+            val firstLaunch = runBlocking { settingsDataStore.getBoolean(FIRST_LAUNCH) ?: true }
+            val settingsState = settingsViewModel.state.collectAsStateWithLifecycle()
+            ExibhiteaseTheme {
+                ExibhiteaseApp(
+                    firstLaunch = firstLaunch,
+                    settingsViewModel = settingsViewModel,
+                    finishActivity = {
+                        finish()
+                    }
+                )
             }
         }
     }
--- app/src/main/java/com/app/exibhitease/presentation/ExibhiteaseApp.kt
+++ app/src/main/java/com/app/exibhitease/presentation/ExibhiteaseApp.kt
@@ -1,18 +1,32 @@
 package com.app.exibhitease.presentation
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
+import com.app.exibhitease.presentation.navigation.Home
 import com.app.exibhitease.presentation.navigation.NavGraph
+import com.app.exibhitease.presentation.navigation.OnBoarding
 import com.app.exibhitease.presentation.settings_screen.SettingsViewModel
 
 @Composable
 fun ExibhiteaseApp(
     firstLaunch : Boolean,
-    settingsViewModel : SettingsViewModel
+    settingsViewModel : SettingsViewModel,
+    finishActivity : () -> Unit
 ){
 
     val navController = rememberNavController()
-
+    NavGraph(
+        modifier = Modifier
+            .fillMaxSize()
+            .navigationBarsPadding(),
+        navController = navController,
+        startDestination = if (firstLaunch) OnBoarding else Home,
+        finishActivity = finishActivity,
+        settingsViewModel = settingsViewModel
+    )
 
 
 }
