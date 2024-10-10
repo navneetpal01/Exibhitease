59f2cf4c2d82e50d9f42d05cf61ff1fbe7213486
navneet <navneetpalgithub@gmail.com>
2024-10-10 16:42:17 +0530

    Updated the OnBoardingScreen.kt

--- app/src/main/java/com/app/exibhitease/presentation/onboarding_screen/OnBoardingScreen.kt
+++ app/src/main/java/com/app/exibhitease/presentation/onboarding_screen/OnBoardingScreen.kt
@@ -1,16 +1,27 @@
 package com.app.exibhitease.presentation.onboarding_screen
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.app.exibhitease.presentation.settings_screen.SettingsEvent
+import com.app.exibhitease.ui.theme.poppins_semiBold
+import com.app.exibhitease.ui.theme.shapphire_blue
 
 @Composable
 fun OnBoardingScreen(
@@ -21,13 +32,7 @@ fun OnBoardingScreen(
         modifier = Modifier
             .fillMaxSize(),
         bottomBar = {
-            Button(
-                onClick = {
-                    onCompleted(SettingsEvent.SetFirstLaunch)
-                }
-            ) {
-                Text(text = "Start")
-            }
+           OnBoardingBottom()
         },
         containerColor = Color.White,
     ){paddingValues ->
@@ -45,4 +50,44 @@ fun OnBoardingScreen(
 
     }
 
+}
+
+@Composable
+fun OnBoardingBottom(){
+    Column(
+        modifier = Modifier
+            .fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ){
+            Button(
+                onClick = {
+
+                },
+                modifier = Modifier
+                    .fillMaxWidth(0.85f)
+                    .padding(vertical = 10.dp),
+                shape = RoundedCornerShape(10.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = shapphire_blue),
+                elevation = null,
+                border = null,
+
+            ) {
+                Text(
+                    text = "Sound exciting \uD83D\uDC4D",
+                    modifier = Modifier
+                        .padding(vertical = 10.dp),
+                    color = Color.White,
+                    fontSize = 15.sp,
+                    fontFamily = poppins_semiBold,
+                )
+            }
+        }
+    }
 }
--- app/src/main/java/com/app/exibhitease/ui/theme/Type.kt
+++ app/src/main/java/com/app/exibhitease/ui/theme/Type.kt
@@ -2,9 +2,13 @@ package com.app.exibhitease.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.app.exibhitease.R
+
+val poppins_semiBold = FontFamily(Font(R.font.poppins_semi_bold))
 
 // Set of Material typography styles to start with
 val Typography = Typography(
